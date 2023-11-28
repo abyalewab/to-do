649bbf52c97b9341562df8bb878062eaba948eaa
abyalew <abyalewab@gmail.com>
2023-11-28 10:53:30 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>To Do List</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
